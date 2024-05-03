f3618c55ce0687b58867397032b29b8c71d6e27e
Zmokyz <Ortizzarif2@gmail.com>
2024-05-03 13:35:44 -0600

    mobile view and svg images added

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const boton_switch = document.querySelector('.switch_mode')
 const parrafos = document.querySelectorAll('p')
 const about = document.querySelector('#header_list li.About');
 const alarado_image = document.getElementById('alarado_image')
+const nav = document.querySelector("#nav")
+const abrir = document.querySelector("#open_menu")
+const cerrar =document.querySelector("#close_menu")
+
 
 boton_switch.addEventListener('click',() => {
   document.body.classList.toggle('dark');
   boton_switch.classList.toggle('active');
   about.classList.toggle('dark');
+  nav.classList.toggle('dark');
+  cerrar.classList.toggle('dark');
+  abrir.classList.toggle('dark_menu');
 
   parrafos.forEach(parrafo => {
     parrafo.classList.toggle('dark');
@@ -23,5 +30,14 @@ boton_switch.addEventListener('click',() => {
   }
 });
 
+abrir.addEventListener('click', () =>{
+  nav.classList.add('extended');
+  cerrar.classList.add('extended');
+  boton_switch.classList.add('boton_menu');
+})
 
-
+cerrar.addEventListener('click', () =>{
+  nav.classList.remove('extended');
+  cerrar.classList.remove('extended');
+  boton_switch.classList.remove('boton_menu');
+})
